bf83d8c859cc9b438c56eea7f9feaae4a99b4046
Georgi <g.borisov123@gmail.com>
2017-10-19 14:35:02 +0300

    Password Encryption

--- Blog/models/user.js
+++ Blog/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
 mongoose.Promise = global.Promise;
 const Schema = mongoose.Schema;
 
@@ -7,7 +8,17 @@ const userSchema = new Schema({
   password: { type: String, required: true, unique: true},
   email:  { type: String, required: true, unique: true},
   
-  
+});
+
+userSchema.pre('save', function(next) {
+  if (!this.isModified('password'))
+    return next();
+
+  bcrypt.hash(this.password, null, null, (err, hash) => {
+    if (err) return next(err); 
+    this.password = hash; 
+    next(); 
+  });
 });
 
 module.exports = mongoose.model('User', userSchema);
